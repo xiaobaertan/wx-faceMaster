fb5b20e45b5fbcada3d56e93647ae797f8bce6f4
Littlebaltan <littlebaltan@163.com>
2019-08-11 18:10:08 +0800

    完成颜值大师

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,11 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const toQueryString = (params) => {
+  return '?' + Object.keys(params).map(i => `${i}=${encodeURIComponent(params[i])}`).join('&')
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  toQueryString
 }
